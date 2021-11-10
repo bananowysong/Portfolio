2ac2f3ee0d04c4b01ef23c6b05d46d6e89633d52
macbookpro <dominik.wojtys@gmail.com>
2021-11-10 11:07:49 +0100

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by MacBook Pro on 10/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
