c93a71e391cc3dfc57d6515f852534df19943a57
macbookpro <dominik.wojtys@gmail.com>
2021-11-20 21:14:13 +0100

    Read awards from JSON

---
+++ Portfolio/AwardsView.swift
@@ -0,0 +1,45 @@
+//
+//  AwardsView.swift
+//  Portfolio
+//
+//  Created by MacBook Pro on 20/11/2021.
+//
+
+import SwiftUI
+
+struct AwardsView: View {
+    static let tag: String? = "Awards"
+    var columns: [GridItem] {
+        [GridItem(.adaptive(minimum: 100, maximum: 100))]
+    }
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(Award.allAwards) { award in
+                        Button {
+
+                        } label: {
+                            Image(systemName: award.image)
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(Color.secondary.opacity(0.5))
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Awards")
+
+            SelectSomethingView()
+        }
+    }
+}
+
+struct AwardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AwardsView()
+    }
+}
