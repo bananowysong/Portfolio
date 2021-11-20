162b398915d2b69a926a63d05979c3b5ae682437
macbookpro <dominik.wojtys@gmail.com>
2021-11-20 19:28:52 +0100

    Finish Project view

--- Portfolio/ProjectsView.swift
+++ Portfolio/ProjectsView.swift
@@ -20,76 +20,84 @@ struct ProjectsView: View {
 
     var body: some View {
         NavigationView {
+            Group {
+                if projects.wrappedValue.count == 0 {
+                    Text("There's nothing here right now.")
+                        .foregroundColor(.secondary)
+                } else {
+                    List {
+                        ForEach(projects.wrappedValue) { project in
+                            Section(header: ProjectHeaderView(project: project)) {
+                                ForEach(project.projectItems(using: sortOrder)) { item in
+                                    ItemRowView(project: project, item: item)
+                                }
+                                .onDelete { offsets in
+                                    let allItems = project.projectItems(using: sortOrder)
 
-            List {
-                ForEach(projects.wrappedValue) { project in
-                    Section(header: ProjectHeaderView(project: project)) {
-                        ForEach(project.projectItems(using: sortOrder)) { item in
-                            ItemRowView(item: item)
-                        }
-                        .onDelete { offsets in
-                            let allItems = project.projectItems
 
+                                    for offset in offsets {
+                                        let item = allItems[offset]
+                                        dataController.delete(item)
+                                    }
 
-                            for offset in offsets {
-                                let item = allItems[offset]
-                                dataController.delete(item)
-                            }
-
-                            dataController.save()
-                        }
-
-                        if showClosedProjects == false {
-                            Button {
-                                withAnimation {
-                                    let item = Item(context: moc)
-                                    item.project = project
-                                    item.creationDate = Date()
                                     dataController.save()
                                 }
+
+                                if showClosedProjects == false {
+                                    Button {
+                                        withAnimation {
+                                            let item = Item(context: moc)
+                                            item.project = project
+                                            item.creationDate = Date()
+                                            dataController.save()
+                                        }
+                                    } label: {
+                                        Label("Add new Item", systemImage: "plus")
+                                    }
+                                }
+                            }
+
+                        }
+
+                    }
+                    .listStyle(InsetGroupedListStyle())
+                    .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            if showClosedProjects == false {
+                                Button {
+                                    withAnimation {
+                                        let project = Project(context: moc)
+                                        project.closed = false
+                                        project.creationDate = Date()
+                                        dataController.save()
+                                    }
+                                } label: {
+                                    Label("Add project", systemImage: "plus")
+                                }
+                            }
+                        }
+
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button {
+                                showingSortOrder.toggle()
                             } label: {
-                                Label("Add new Item", systemImage: "plus")
+                                Label("Sort", systemImage: "arrow.up.arrow.down")
                             }
                         }
+
                     }
-
-                }
-
-            }
-            .listStyle(InsetGroupedListStyle())
-            .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    if showClosedProjects == false {
-                        Button {
-                            withAnimation {
-                                let project = Project(context: moc)
-                                project.closed = false
-                                project.creationDate = Date()
-                                dataController.save()
-                            }
-                        } label: {
-                            Label("Add project", systemImage: "plus")
-                        }
+                    .actionSheet(isPresented: $showingSortOrder) {
+                        ActionSheet(title: Text("Sort items"), message: nil, buttons: [
+                            .default(Text("Optimized")) { sortOrder = .optimized },
+                            .default(Text("Creation Date")) { sortOrder = .creationDate },
+                            .default(Text("Title")) { sortOrder = .title }
+                        ])
                     }
                 }
-
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        showingSortOrder.toggle()
-                    } label: {
-                        Label("Sort", systemImage: "arrow.up.arrow.down")
-                    }
-                }
-
             }
-            .actionSheet(isPresented: $showingSortOrder) {
-                ActionSheet(title: Text("Sort items"), message: nil, buttons: [
-                    .default(Text("Optimized")) { sortOrder = .optimized },
-                    .default(Text("Creation Date")) { sortOrder = .creationDate },
-                    .default(Text("Title")) { sortOrder = .title }
-                ])
-            }
+            SelectSomethingView()
+
 
         }
     }
