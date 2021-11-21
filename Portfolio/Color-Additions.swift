3d7f20e0af6989ded9f89f3b2585cd7de8d53f07
macbookpro <dominik.wojtys@gmail.com>
2021-11-21 14:01:15 +0100

    Create the home view

---
+++ Portfolio/Color-Additions.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Portfolio
+//
+//  Created by MacBook Pro on 21/11/2021.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
+
+
