a1c556338994b0cfbb19f6c15c2fd53d02a8bf71
macbookpro <dominik.wojtys@gmail.com>
2021-11-21 11:33:07 +0100

    Count core data results for rewards

--- Portfolio/AwardsView.swift
+++ Portfolio/AwardsView.swift
@@ -12,34 +12,47 @@ struct AwardsView: View {
     var columns: [GridItem] {
         [GridItem(.adaptive(minimum: 100, maximum: 100))]
     }
-    
+    @EnvironmentObject var dataController: DataController
+    @State private var selectedAward = Award.example
+    @State private var showingAwardDetails = false
+
     var body: some View {
         NavigationView {
             ScrollView {
                 LazyVGrid(columns: columns) {
                     ForEach(Award.allAwards) { award in
                         Button {
-
+                            selectedAward = award
+                            showingAwardDetails = true
                         } label: {
                             Image(systemName: award.image)
                                 .resizable()
                                 .scaledToFit()
                                 .padding()
                                 .frame(width: 100, height: 100)
-                                .foregroundColor(Color.secondary.opacity(0.5))
+                                .foregroundColor(dataController.hasEearned(award: award) ? Color(award.color) : Color.secondary.opacity(0.5))
                         }
                     }
                 }
             }
             .navigationTitle("Awards")
-
+            .alert(isPresented: $showingAwardDetails) {
+                if dataController.hasEearned(award: selectedAward) {
+                    return Alert(title: Text("Unlocked: \(selectedAward.name)"), message: Text(selectedAward.description), dismissButton: .default(Text("OK")))
+                } else {
+                    return Alert(title: Text("Locked"), message: Text(selectedAward.description), dismissButton: .default(Text("OK")))
+                }
+            }
             SelectSomethingView()
         }
     }
 }
 
 struct AwardsView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+
     static var previews: some View {
         AwardsView()
+            .environmentObject(dataController)
     }
 }
