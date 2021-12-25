391625b71a3809040160feb88fd4f913ced5b89e
macbookpro <dominik.wojtys@gmail.com>
2021-12-25 12:06:54 +0100

    Add some doccumentation

--- Portfolio/PortfolioApp.swift
+++ Portfolio/PortfolioApp.swift
@@ -16,6 +16,10 @@ struct PortfolioApp: App {
             ContentView()
                 .environment(\.managedObjectContext, dataController.container.viewContext)
                 .environmentObject(dataController)
+            // Automatically save when we detect that we are
+            // no longer the foreground app. Use this rather than
+            // scene phase so we can port to macOS, where scene
+            // phase won't detect our app losing focus.
                 .onReceive(
                     NotificationCenter.default.publisher(
                         for: UIApplication.willResignActiveNotification),
